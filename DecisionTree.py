5e493e1c5f2d2f5d34bb4ab56cbb14931147443e
lrm25 <luke.r.mccrone@gmail.com>
2018-11-04 15:36:14 -0700

    Issue #6:  quit option

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,8 +2,9 @@ from Question import Question
 
 class DecisionTree:
 
-    def __init__(self):
-        self.main_question = Question(1, "What do you want out of life?", \
-                                      ("Nothing", "Everything"))
-        self.main_question.ask()
-        self.main_question.get_answer()
+    def __init__(self, name):
+        self.name = name
+        #self.main_question = Question(1, "What do you want out of life?", \
+        #                              ("Nothing", "Everything"))
+        #self.main_question.ask()
+        #self.main_question.get_answer()
--- materialsList.py
+++ materialsList.py
@@ -1,7 +1,54 @@
 from DecisionTree import DecisionTree
 
+main_options = { "1" : "Generate materials list", \
+                 "2" : "Create/edit tree", \
+                 "3" : "Backup", \
+                 "4" : "Quit" }
+
+existing_trees = { "1" : "Sample" }
+
 def main():
-    tree = DecisionTree()
+    while True:
+        main_loop()
+
+def create_edit_tree():
+    option_selected = False
+    for option in existing_trees:
+        print(option + ". " + existing_trees[option])
+    while not option_selected:
+        selected_option = input("Select tree, or (c)reate new:  ")
+        if selected_option.lower() != 'c' and \
+           not selected_option in existing_trees:
+            print("Invalid selection")
+        else:
+            option_selected = True
+    if selected_option.lower() == 'c':
+        create_tree()
+
+def create_tree():
+    valid_tree = False
+    tree_name = input("Enter tree name: ")
+    tree = DecisionTree(tree_name)
+    print("Created new tree with name " + tree.name)
+        
+
+def main_loop():
+    option_selected = False
+    for option in main_options:
+        print(option + ". " + main_options[option])
+    while not option_selected:
+        selected_option = input("Enter value: ")
+        if not selected_option in main_options:
+            print("Invalid selection")
+        else:
+            option_selected = True
+    if selected_option == '2':
+        create_edit_tree()
+    elif selected_option == '4':
+        quit()
+    else:
+        print("Not implemented")
+    #tree = DecisionTree()
 
 if __name__ == "__main__":
     main()
