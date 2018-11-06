eaa45af37579321f0a49daf1d198977c76ae440e
lrm25 <luke.r.mccrone@gmail.com>
2018-11-06 15:40:57 -0700

    Issue #14 - full option loop unit test

--- src/materials_list.py
+++ src/materials_list.py
@@ -1,6 +1,9 @@
 import sqlite3
 
 EMPTY_OPTION_DICT="Options dictionary is empty"
+EMPTY_OPTION_STR="Empty option selection string"
+INVALID_ACCEP_VAL="Invalid additional option"
+INVALID_SEL_STR="Invalid selection"
 
 from decision_tree import DecisionTree, retrieveAll
 
@@ -17,16 +20,28 @@ def select_option(option_dict, input_str, acceptable = ()):
 
     if option_dict is None or len(option_dict) is 0: 
         raise Exception(EMPTY_OPTION_DICT)
+
+    if input_str is None or len(input_str) is 0 or input_str.isspace():
+        raise Exception(EMPTY_OPTION_STR)
+
+    for also_allowed in acceptable:
+        if also_allowed is None or len(also_allowed) is 0 or \
+           also_allowed.isspace():
+            raise Exception(INVALID_ACCEP_VAL)
+
     option_selected = False
     for option in option_dict:
         print(option + ". " + option_dict[option])
+
     while not option_selected:
-        selected_option = input(input_str)
+        print(input_str, end=" ")
+        selected_option = input()
         if not selected_option in acceptable and \
            not selected_option in option_dict:
-            print("Invalid selection")
+            print(INVALID_SEL_STR)
         else:
             option_selected = True
+
     return selected_option
 
 def create_edit_tree():
--- tests/test_materials_list.py
+++ tests/test_materials_list.py
@@ -1,12 +1,90 @@
+import os, sys
+import mock
 import pytest
+from io import StringIO
 
 import materials_list
 
-def test_select_option():
+def test_select_option_1():
     with pytest.raises(Exception) as excinfo:
         # No options
         option_dict = {}
         input_str = ""
         materials_list.select_option(option_dict, input_str)
     assert materials_list.EMPTY_OPTION_DICT in str(excinfo.value)
+
+def test_select_option_2():
+    with pytest.raises(Exception) as excinfo:
+        # No options
+        option_dict = None
+        input_str = ""
+        materials_list.select_option(option_dict, input_str)
+    assert materials_list.EMPTY_OPTION_DICT in str(excinfo.value)
     
+def test_select_option_3():
+    with pytest.raises(Exception) as excinfo:
+        option_dict = {'1': 'test'}
+        input_str = None
+        materials_list.select_option(option_dict, input_str)
+    assert materials_list.EMPTY_OPTION_STR in str(excinfo.value)
+
+def test_select_option_4():
+    with pytest.raises(Exception) as excinfo:
+        option_dict = {'1': 'test'}
+        input_str = ""
+        materials_list.select_option(option_dict, input_str)
+    assert materials_list.EMPTY_OPTION_STR in str(excinfo.value)
+
+def test_select_option_5():
+    with pytest.raises(Exception) as excinfo:
+        option_dict = {'1': 'test'}
+        input_str = "\n"
+        materials_list.select_option(option_dict, input_str)
+    assert materials_list.EMPTY_OPTION_STR in str(excinfo.value)
+
+def test_select_option_6():
+    with pytest.raises(Exception) as excinfo:
+        option_dict = {'1': 'test'}
+        input_str = "test"
+        acceptable = ( None, "a" )
+        materials_list.select_option(option_dict, input_str, acceptable)
+    assert materials_list.INVALID_ACCEP_VAL in str(excinfo.value)
+
+def test_select_option_7():
+    with pytest.raises(Exception) as excinfo:
+        option_dict = {'1': 'test'}
+        input_str = "test"
+        acceptable = ( "\r", "a" )
+        materials_list.select_option(option_dict, input_str, acceptable)
+    assert materials_list.INVALID_ACCEP_VAL in str(excinfo.value)
+        
+def test_select_option_8(capsys):
+        
+        o_id='1'
+        o_desc='test'
+        t_id='2'
+        t_desc='test2'
+        input_str='input:'
+        input_value = ['3', '1', 'a', 'b']
+
+        o_line=o_id+". "+o_desc+'\n'
+        t_line=t_id+". "+t_desc+'\n'
+        bad_input=input_str+" "+materials_list.INVALID_SEL_STR+'\n'
+
+        def mock_input():
+            return input_value.pop(0)
+        materials_list.input = mock_input
+        option_dict = { o_id : o_desc, t_id : t_desc }
+        acceptable = ("a", "b")
+        # Invalid input, followed by valid
+        materials_list.select_option(option_dict, input_str, acceptable)
+        capture = capsys.readouterr()
+        assert capture.out == o_line+t_line+bad_input+input_str+" "
+        # Valid additional input one
+        materials_list.select_option(option_dict, input_str, acceptable)
+        capture = capsys.readouterr()
+        assert capture.out == o_line+t_line+input_str+" "
+        # Valid additional input two
+        materials_list.select_option(option_dict, input_str, acceptable)
+        capture = capsys.readouterr()
+        assert capture.out == o_line+t_line+input_str+" "
